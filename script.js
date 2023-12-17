dd39b2b88cf30202f89225c75f573c3422b831cd
cc0803 <simongeldbach@gmail.com>
2023-12-17 17:43:21 +0100

    Create fields array and functions for populating the graph

---
+++ script.js
@@ -0,0 +1,110 @@
+// Chessboard fields
+let fields = [
+	[0, 0],
+	[0, 1],
+	[0, 2],
+	[0, 3],
+	[0, 4],
+	[0, 5],
+	[0, 6],
+	[0, 7],
+	[1, 0],
+	[1, 1],
+	[1, 2],
+	[1, 3],
+	[1, 4],
+	[1, 5],
+	[1, 6],
+	[1, 7],
+	[2, 0],
+	[2, 1],
+	[2, 2],
+	[2, 3],
+	[2, 4],
+	[2, 5],
+	[2, 6],
+	[2, 7],
+	[3, 0],
+	[3, 1],
+	[3, 2],
+	[3, 3],
+	[3, 4],
+	[3, 5],
+	[3, 6],
+	[3, 7],
+	[4, 0],
+	[4, 1],
+	[4, 2],
+	[4, 3],
+	[4, 4],
+	[4, 5],
+	[4, 6],
+	[4, 7],
+	[5, 0],
+	[5, 1],
+	[5, 2],
+	[5, 3],
+	[5, 4],
+	[5, 5],
+	[5, 6],
+	[5, 7],
+	[6, 0],
+	[6, 1],
+	[6, 2],
+	[6, 3],
+	[6, 4],
+	[6, 5],
+	[6, 6],
+	[6, 7],
+	[7, 0],
+	[7, 1],
+	[7, 2],
+	[7, 3],
+	[7, 4],
+	[7, 5],
+	[7, 6],
+	[7, 7],
+];
+
+// Graph as adjacency list
+let graph = new Map();
+
+// Adding Notes
+function addNote(position) {
+	graph.set(position, []);
+}
+
+// Adding vertices
+function addVertex(position, value) {
+	graph.get(position).push(value);
+	graph.get(value).push(position);
+}
+
+fields.forEach((element) => {
+	addNote(element);
+});
+
+console.log(graph);
+
+function createPossibleVertices(point) {
+	let x = point[0];
+	let y = point[1];
+
+	let possibilities = [];
+
+	// Create all posibilites
+	possibilities.push([x + 2, y + 1]);
+	possibilities.push([x - 2, y + 1]);
+	possibilities.push([x + 1, y + 2]);
+	possibilities.push([x - 1, y + 2]);
+	possibilities.push([x - 2, y - 1]);
+	possibilities.push([x + 2, y - 1]);
+	possibilities.push([x + 1, y - 2]);
+	possibilities.push([x - 1, y - 2]);
+
+	let results = [];
+
+	return results;
+}
+
+console.log(createPossibleVertices([0, 0]));
